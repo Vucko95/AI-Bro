3a81362c3350e57686e0dc3f38b992935d688fda
Ronald Suplina <tyger995@gmail.com>
2023-09-04 17:06:43 +0100

    Add Base Elements

---
+++ ai-bro/components/Navbar.tsx
@@ -0,0 +1,15 @@
+"use clinet";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+export const Navbar = () => {
+    return (
+        <div className="fixed w-full  z-50 flex justify-between items-center bg-secondary py-2 px-4 border-b-2">
+            <Link href="/">
+            <h1 className=" hidden md:block text-2xl ">AI BRO</h1>
+            </Link>
+            <div className="flex items-center">
+                <Menu className="block md:hidden" />
+            </div>
+        </div>
+    )
+}
